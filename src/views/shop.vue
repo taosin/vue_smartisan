9f91a32af178163bafb3dd5c723862d3822679d7
iMocco <taoxin167@gmail.com>
2017-11-09 16:50:30 +0800

    调整页面样式

--- src/views/shop.vue
+++ src/views/shop.vue
@@ -53,106 +53,111 @@
 	</div>
 </template>
 <script>
-	export default {
-		components:{
-		},
-		data () {
-			return {
-				img1:'http://image.smartisanos.cn/resource/b/8/J1.png?x-oss-process=image/resize,w_1220/quality,Q_100/format,webp',
-				img2:'http://image.smartisanos.cn/resource/e/3/J2.png?x-oss-process=image/resize,w_1220/quality,Q_100/format,webp',
-				img3:'http://image.smartisanos.cn/resource/2/8/J3.png?x-oss-process=image/resize,w_1220/quality,Q_100/format,webp'
-			}
-		},
-		mounted(){
-		},
-		methods:{
+export default {
+	components:{
+	},
+	data () {
+		return {
+			img1:'http://image.smartisanos.cn/resource/b/8/J1.png?x-oss-process=image/resize,w_1220/quality,Q_100/format,webp',
+			img2:'http://image.smartisanos.cn/resource/e/3/J2.png?x-oss-process=image/resize,w_1220/quality,Q_100/format,webp',
+			img3:'http://image.smartisanos.cn/resource/2/8/J3.png?x-oss-process=image/resize,w_1220/quality,Q_100/format,webp'
 		}
+	},
+	mounted(){
+	},
+	methods:{
 	}
+}
 </script>
 <style scoped lang="less">
-	.s-shop{
-		width: 100%;
-		text-align: center;
-		background-color: #ededed;
-		margin-top: 1px;
-		padding-top: 50px;
-		height: 1400px;
-		.s-banner{
-			width: 1220px;
-			height: 500px;
-			margin: 0 auto;
-			margin-bottom: 20px;
-			.s-banner-items{
-				.s-banner-item{
+.s-shop{
+	width: 100%;
+	text-align: center;
+	background-color: #ededed;
+	margin-top: 1px;
+	padding-top: 50px;
+	height: 1400px;
+	.s-banner{
+		width: 1220px;
+		height: 500px;
+		margin: 0 auto;
+		margin-bottom: 20px;
+		.s-banner-items{
+			.s-banner-item{
+				position: absolute;
+				z-index: 4;
+				border: 1px solid #ddd;
+				width: 1220px;
+				height: 500px;
+				border-radius: 10px;
+				.s-img1{
 					position: absolute;
-					z-index: 4;
-					border: 1px solid #ddd;
-					width: 1220px;
-					height: 500px;
-					border-radius: 10px;
-					.s-img1{
-						position: absolute;
-						z-index: 1;
-					}
-					.s-img2{
-						position: absolute;
-						z-index: 2;
-					}
-					.s-img3{
-						position: absolute;
-						z-index: 3;
-					}
-					.s-img1,.s-img2, .s-img3{
-						margin-top: -27px;
-						img{
-							border-radius: 49px;
-						}
-					}
+					z-index: 1;
 				}
-				.s-paganition{
+				.s-img2{
 					position: absolute;
-					z-index: 5;
-					width: 100px;
-					height: 30px;
-					bottom: 113px;
-					left: 560px;
-					text-align: center;
-					ul{
-						list-style: none;
-						padding: 0;
-						li{
-							float: left;
-							padding:0 5px;
-							.s-p-s{
-								width: 10px;
-								height: 10px;
-								background: red;
-								border-radius: 5px;
-								display: inline-block;
-							}
-						}
+					z-index: 2;
+				}
+				.s-img3{
+					position: absolute;
+					z-index: 3;
+				}
+				.s-img1,.s-img2, .s-img3{
+					margin-top: -27px;
+					img{
+						border-radius: 49px;
 					}
 				}
 			}
-		}
-		.s-1{
-			margin-top: 30px;
-			width: 1220px;
-			margin-left: auto;
-			margin-right: auto;
-			ul{
-				list-style: none;
-				padding:0;
-				li{
-					float: left;
-					width: 25%;
-					img{
-						width: 100%;
-						height: 100%;
-						border: 1px solid;
+			.s-paganition{
+				position: absolute;
+				z-index: 5;
+				width: 100px;
+				height: 30px;
+				bottom: 113px;
+				left: 560px;
+				text-align: center;
+				ul{
+					list-style: none;
+					padding: 0;
+					li{
+						float: left;
+						padding:0 5px;
+						.s-p-s{
+							width: 10px;
+							height: 10px;
+							background: red;
+							border-radius: 5px;
+							display: inline-block;
+						}
 					}
 				}
 			}
 		}
 	}
+	.s-1{
+		margin-top: 30px;
+		width: 1220px;
+		margin-left: auto;
+		margin-right: auto;
+		ul{
+			list-style: none;
+			padding:0;
+			li{
+				float: left;
+				width: 25%;
+				cursor: pointer;
+				img{
+					width: 100%;
+					height: 100%;
+					border: 1px solid #ddd;
+				}
+				img:hover{
+					opacity: .4;
+					transition:.5s;
+				}
+			}
+		}
+	}
+}
 </style>
