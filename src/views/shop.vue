9b306916bc6fbdc283c0c33d7ccdeab88750bdf9
iMocco <taoxin167@gmail.com>
2017-12-04 14:32:11 +0800

    修改热门商品模块

--- src/views/shop.vue
+++ src/views/shop.vue
@@ -20,6 +20,7 @@
 				</div>
 			</div>
 		</div>
+		<!-- 推荐商品 -->
 		<div class="s-1">
 			<ul class="s-1-ul">
 				<li class="s-1-ul-li">
@@ -35,6 +36,32 @@
 					<img class="s-1-ul-li-img" src="http://image.smartisanos.cn/resource/1334fa8aea2a311289d8154dd7a26efc.jpg?x-oss-process=image/resize,w_610/quality,Q_100/format,webp">
 				</li>
 			</ul>
+		</div>
+
+		<!-- 热门商品 -->
+		<div class="s-hot">
+			<div class="s-hot-title">热门商品</div>
+			<div class="s-hot-main">
+				<ul>
+					<li v-for="hot in hots">
+						<img :src="hot.img">
+						<p class="name">{{hot.name}}</p>
+						<p class="desc">{{hot.desc}}</p>
+						<div class="colors" v-if="hot.colors && hot.colors.length">
+							<div class="item" v-for="color in hot.colors">
+								<div :style="{background:color}" class="item-color"></div>
+							</div>
+						</div>
+						<div class="price">
+							<p>{{hot.price}}</p>
+							<div class="btns-class">
+								<a class="btn btn-detail">查看详情</a>
+								<a class="btn btn-cart">加入购物车</a>
+							</div>
+						</div>
+					</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </template>
@@ -68,7 +95,40 @@ export default {
       rotateY: 0,
       shadowX: 0,
       shadowY: 0,
-      blur: 0
+      blur: 0,
+      hots: [
+        {
+          img:
+            "https://resource.smartisan.com/resource/a31af6371f478ef77b26500b9863f59a.png?x-oss-process=image/resize,w_412/format,webp/quality,Q_90",
+          price: "￥99.00",
+          name: "“足迹系列”保护套 照相机发明者达盖尔出生",
+          desc: "超强净化能力，超低噪音，智能操控"
+        },
+        {
+          img:
+            "https://resource.smartisan.com/resource/71432ad30288fb860a4389881069b874.png?x-oss-process=image/resize,w_412/format,webp/quality,Q_90",
+          price: "￥3499.00",
+          name: "畅呼吸智能空气净化器",
+          desc: "超强净化能力，超低噪音，智能操控",
+          colors: ["#fff"]
+        },
+        {
+          img:
+            "https://resource.smartisan.com/resource/3bd5267edd7257e719e7965b756e2c2e.png?x-oss-process=image/resize,w_412/format,webp/quality,Q_90",
+          price: "￥1299.00",
+          name: "坚果 Pro",
+          desc: "购机最高优惠500元",
+          colors: ["#333", "red", "#888", "#fff", "#e32"]
+        },
+        {
+          img:
+            "https://resource.smartisan.com/resource/d9a4019f575264674c7826645ca7d33a.jpg?x-oss-process=image/resize,w_412/format,webp/quality,Q_90",
+          price: "￥99.00",
+          name: "Smartisan 半入耳式耳机 心动版",
+          desc: "哑光表面、专业级调音",
+          colors: ["blue", "red", "#333"]
+        }
+      ]
     };
   },
   mounted() {},
@@ -78,11 +138,11 @@ export default {
         event.pageX - event.target.offsetWidth / 2 - event.target.offsetLeft;
       var baseY =
         event.pageY - event.target.offsetHeight / 2 - event.target.offsetTop;
-			this.rotateX = baseY / -30;
-			this.rotateY = baseX / 70;
+      this.rotateX = baseY / -30;
+      this.rotateY = baseX / 70;
       this.shadowX = baseX / -30;
-      this.shadowY = baseY / -10;;
-			this.blur = 30;
+      this.shadowY = baseY / -10;
+      this.blur = 30;
     },
     mousemout() {
       this.rotateX = 0;
@@ -176,6 +236,8 @@ export default {
     width: 1220px;
     margin-left: auto;
     margin-right: auto;
+    margin-bottom: 30px;
+    overflow: hidden;
     ul {
       list-style: none;
       padding: 0;
@@ -189,8 +251,135 @@ export default {
           border: 1px solid #ddd;
         }
         img:hover {
-          opacity: 0.4;
+          opacity: 0.7;
           transition: 0.5s;
+        }
+      }
+    }
+  }
+
+  .s-hot {
+    margin: 0 auto;
+    width: 1220px;
+    border: 1px solid #ddd;
+    overflow: hidden;
+    margin-top: 30px;
+    background: #fff;
+    text-align: left;
+    border-radius: 8px;
+    .s-hot-title {
+      font-size: 18px;
+      padding: 15px 0;
+      margin-left: 30px;
+      color: #888;
+    }
+    .s-hot-main {
+      text-align: center;
+      border-top: 1px solid #ddd;
+      ul {
+        margin: 0;
+        padding: 0;
+        li {
+          float: left;
+          list-style: none;
+          width: 304px;
+          border-left: 1px solid #f2f2f2;
+          padding-top: 40px;
+          cursor: pointer;
+          img {
+            width: 70%;
+          }
+          .name {
+            font-size: 16px;
+            box-sizing: border-box;
+            width: 100%;
+            padding: 0px 10px;
+            overflow: hidden;
+            font-size: 16px;
+            line-height: 1.2;
+            white-space: nowrap;
+            text-overflow: ellipsis;
+            color: #424242;
+          }
+          .desc {
+            color: #e04e4e;
+            border: 1ox solid;
+            font-weight: 500;
+          }
+          .colors {
+            position: absolute;
+            border: 1px solid;
+            // margin: 0 auto;
+            text-align: center;
+            .item {
+							float: left;
+              .item-color {
+                width: 20px;
+                height: 20px;
+              }
+            }
+          }
+          .price {
+            position: relative;
+            top: 30px;
+            margin-bottom: 60px;
+            font-weight: 800;
+            display: inline-block;
+            width: 100%;
+            font-family: Arial;
+            line-height: 1.2;
+            text-align: center;
+            p {
+              color: #de4037;
+              font-size: 18px;
+              display: block;
+            }
+          }
+          .btns-class {
+            display: none;
+            transition: all 0.15s ease;
+            margin: 14px 0;
+            .btn {
+              display: inline-block;
+              border: 1px solid #e1e1e1;
+              border-radius: 4px;
+              font-size: 12px;
+              color: #646464;
+              background-color: #f9f9f9;
+              background-image: linear-gradient(180deg, #fff, #f9f9f9);
+              transition: all 0.15s ease-out;
+              text-decoration: none;
+              width: 98px;
+              padding: 7px 0;
+              margin: 0 2px;
+            }
+            .btn-cart {
+              border: 1px solid #5c81e3;
+              color: #fff;
+              background: #678ee7;
+              background-image: linear-gradient(180deg, #678ee7, #5078df);
+            }
+            .btn-detail:hover {
+              background-color: #eee;
+              background-image: linear-gradient(180deg, #f5f5f5, #eee);
+            }
+            .btn-cart:hover {
+              background-color: #6c8cd4;
+              background-image: linear-gradient(180deg, #6c8cd4, #4769c2);
+            }
+          }
+        }
+        li:hover {
+          box-shadow: inset 0 0 38px rgba(0, 0, 0, 0.08);
+          transition: all 0.15s ease;
+          .btns-class {
+            display: block;
+          }
+          .price {
+            p {
+              display: none;
+            }
+          }
         }
       }
     }
